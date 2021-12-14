81ea3af87f48c319f7b5fe709ce3bacbb5e003aa
Eugene <>
2021-12-14 12:28:33 +0200

    Array: Safe subscript with Range.

--- Sources/Foundatee/Array+Extension.swift
+++ Sources/Foundatee/Array+Extension.swift
@@ -90,3 +90,20 @@ extension Array {
         return index >= 0 && index < endIndex ? self[index] : nil
     }
 }
+
+extension Array where Element : Equatable {
+    
+    public subscript(safe bounds: Range<Int>) -> ArraySlice<Element> {
+        if bounds.lowerBound > count { return [] }
+        let lower = Swift.max(0, bounds.lowerBound)
+        let upper = Swift.max(0, Swift.min(count, bounds.upperBound))
+        return self[lower..<upper]
+    }
+  
+    public subscript(safe lower: Int?, _ upper: Int?) -> ArraySlice<Element> {
+        let lower = lower ?? 0
+        let upper = upper ?? count
+        if lower > upper { return [] }
+        return self[safe: lower..<upper]
+    }
+}
