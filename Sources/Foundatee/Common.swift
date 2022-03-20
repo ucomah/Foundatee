0707834cf15e4811196ea6cd9f506347042076ee
Eugene <>
2022-03-20 15:19:16 +0200

    Debugger detection.

--- Sources/Foundatee/Common.swift
+++ Sources/Foundatee/Common.swift
@@ -7,3 +7,24 @@ public func _is_Simulator() -> Bool {
     return false
     #endif
 }
+
+public func isDebuggerAttached() -> Bool {
+    #if os(iOS) || os(tvOS)
+    if let _ = ProcessInfo.processInfo.environment["OS_ACTIVITY_DT_MODE"]?.boolValue {
+        return true
+    }
+    return false
+    #else
+    // Buffer for "sysctl(...)" call's result.
+    var info = kinfo_proc()
+    // Counts buffer's size in bytes (like C/C++'s `sizeof`).
+    var size = MemoryLayout.stride(ofValue: info)
+    // Tells we want info about own process.
+    var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+    // Call the API (and assert success).
+    let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
+    assert(junk == 0, "sysctl failed")
+    // Finally, checks if debugger's flag is present yet.
+    return (info.kp_proc.p_flag & P_TRACED) != 0
+    #endif
+}
